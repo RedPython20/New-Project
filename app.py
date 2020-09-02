d1a220ea90c05621139589c95d71fa4c4a0f875c
RedPython20 <66254093+RedPython20@users.noreply.github.com>
2020-09-02 10:59:18 +0100

    Biggest number
    Find the biggest number in a list regardless of position

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
-numbers = [1, 1, 5]
-for x_count in numbers:
-    output = ''
-    for count in range(x_count):
-        output += 'x'
-    print(output)
+numbers = [1, 2 , 3, 4, 5, 6, 7]
+max = numbers[0]
+for number in numbers:
+    if number > max:
+        max = number
+
+print(max)
