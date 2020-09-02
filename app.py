6927852e8a9242ab8a70c1ee1ed6d43ab10b0a0e
RedPython20 <66254093+RedPython20@users.noreply.github.com>
2020-09-02 10:20:38 +0100

    X program
    make an 'x' or 'L' shape

--- app.py
+++ app.py
@@ -1,28 +1,6 @@
-# Car Game
-command = ""
-started = False
-while True:
-    command = input("> ").lower()
-    if command == "start":
-        if started:
-            print("Dude,car is already started.")
-        else:
-            started = True
-            print("Car started! Let's go!")
-
-    elif command == "stop":
-        if not started:
-            print("Car's already stopped!")
-        else:
-            started = False
-            print("Car stopped.")
-    elif command == "help":
-        print("""
-start - starts the car
-stop - stops the car
-quit - exits the game
-        """)
-    elif command == "quit":
-        break
-    else:
-        print("I don't understand that...")
+numbers = [1, 1, 5]
+for x_count in numbers:
+    output = ''
+    for count in range(x_count):
+        output += 'x'
+    print(output)
